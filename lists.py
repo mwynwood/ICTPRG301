53ae733d6917cacebf88df6ca227c51afc339bfb
Marcus Wynwood <marcus.wynwood@education.tas.gov.au>
2019-10-24 11:07:20 +1100

    Added list.append and list.extend

--- lists.py
+++ lists.py
@@ -5,35 +5,42 @@ myList = []
 
 # Make a list of integers
 myList = [42, 105, 234, 142, 49, 25, 69]
+
 # Print the list
 print(myList)
+
 # Sort the list
 myList.sort()
+
+# Add an item to the list
+myList.append("We are the Borg")
+
+# Add multiple items in one go
+myList.extend(["Resistance", "is", "futile"])
+
 # Print the list
 print(myList)
 
 # Count the length of a list
-print(len(myList))
-
-# Make a list with mixed datatypes
-myList = [69, "Hello", 3.14]
-# Print the list
-print(myList)
+print("List Length:", len(myList))
 
 # Print the item at position 1
 print(myList[1])
 
 # Change the item in position 0
 myList[0] = 42
+
 # Print the list
 print(myList)
 
 # Remove the item in position 2
 myList.pop(2)
+
 # Print the list
 print(myList)
 
 # Remove an item by value
-myList.remove("Hello")
+myList.remove(42)
+
 # Print the list
 print(myList)
