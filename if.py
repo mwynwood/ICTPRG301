4d6c6865587c481b5d39f23ee94537edb5a384b7
mwynwood <marcus.wynwood@education.tas.gov.au>
2019-10-25 15:55:58 +1100

    Added examples of OR and AND

--- if.py
+++ if.py
@@ -12,12 +12,20 @@
 
 a = 7
 b = 42
+c = 3.14
 
 if a == b:
     print("a is equivalent to b")
 
 if a < b:
     print("a is less than b")
+
+# Check for multiple conditions using AND and OR
+if a > b or a > c:
+    print("a is greater than b OR a is greater than c")
+
+if c < a and c < b:
+    print("c is less than a AND c is also less than b")
 
 # You can optionally include an ELSE clause to run code if the CONDITION is False:
 age = 17
